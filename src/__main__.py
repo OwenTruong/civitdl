59bc75e7b5b9ebb1410803b790361dfa36d46d2d
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-08-24 14:37:46 -0400

    added crude functionalities for downloading model data and metadata from civitai api

---
+++ src/__main__.py
@@ -0,0 +1,5 @@
+import sys
+from get_lora_api import get_model
+
+
+get_model(sys.argv[1])
--- src/get_lora_api.py
+++ src/get_lora_api.py
@@ -3,31 +3,51 @@ from pathlib import Path
 import requests
 import urllib.request
 import sys
+import os
 
 from utils.utils import write_to_file
 
 
-def download_model(id: str):
-    url = 'https://civitai.com/api/download/models/' + id
-    response = requests.get(url)
-
-
-def get_model_metadata(id: str):
+def get_metadata_json(id: str):
+    """Returns json object if request succeeds, else print error and returns None"""
     metadata_url = 'https://civitai.com/api/v1/models/' + id
-    model_url = 'https://civitai.com/api/download/models/' + id
-
     meta_res = requests.get(metadata_url)
-    model_res = requests.get(model_url)
-
     if (meta_res.status_code != 200):
-        print(f'Error: Metadata Status {meta_res.status_code}')
+        print(f'Error: Fetching metadata failed ({meta_res.status_code})')
+    else:
+        return meta_res.json()
+
+
+def get_model(model_id: str, version_id: str = None):
+    def get_model_url(version):
+        return f'https://civitai.com/api/download/models/{version}'
+
+    # Fetch model metadata
+    meta_json = get_metadata_json(model_id)
+    if (meta_json == None):
         return
-    if (model_res.status_code != 200):
-        print(f'Error: Model Status {model_res.status_code}')
-        return
 
-    write_to_file('test.json', dumps(meta_res.json()))
-    write_to_file('test.safetensors', model_res.content, 'wb')
+    # Find the specific version of the model
+    models_obj: list = meta_json['modelVersions']
+    model_obj = models_obj[0] if version_id == None else next(
+        (obj for obj in models_obj if obj['id' == version_id]), None)
+    if (model_obj == None):
+        return print('Error: The version id provided does not exist for this model')
 
+    # Fetch model data
+    model_res = requests.get(get_model_url(model_obj['id']))
+    if model_res.status_code != 200:
+        return print(f"Error: Fetching model failed ({model_res.status_code})")
 
-get_model_metadata(sys.argv[1])
+    # Find filename
+    content_disposition = model_res.headers.get('Content-Disposition')
+    if (content_disposition == None):
+        return print(f"Error: No Content Disposition Available")
+    filename = os.path.splitext(
+        content_disposition.split('filename=')[-1].strip('"')
+    )[0]
+
+    # Write metadata and model data to files
+    write_to_file(f'manual-test/{filename}.json', dumps(meta_json))
+    write_to_file(
+        f'manual-test/{filename}.safetensors', model_res.content, 'wb')
