a63edad822581b120ce223654ce9e9607b039cbd
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-11-11 22:03:49 -0500

    added parsing support for civitai.com/api urls

--- src/civitai_batch_download/get_model.py
+++ src/civitai_batch_download/get_model.py
@@ -2,7 +2,7 @@ from json import dumps
 import requests
 import os
 import re
-from typing import Callable, Dict
+from typing import Callable, Dict, List
 
 from termcolor import colored
 
@@ -24,18 +24,18 @@ class Metadata:
     nsfw = False
 
     def __init__(self, id_string: str):
-        err_501_if_true('/api/' in id_string,
-                        'API endpoint is not accepted yet')
         self.__id_string = id_string
 
         if id_string.isdigit():
             self.__id_handler()
+        elif "/api/" in id_string:
+            self.__api_url_handler()
         else:
             self.__url_handler()
 
     def __id_handler(self):
         id = [self.__id_string]
-        self.__handler(id)
+        self.__handler(id, 'id')
 
     def __url_handler(self):
         model_id_regex = r'(?<=models\/)\d+'
@@ -52,31 +52,42 @@ class Metadata:
             version_id = version_id.group(0)
             err_500_if_true(
                 version_id == None, f'Unknown error while parsing version id for the url/id provided: {self.__id_string}')
-            self.__handler([model_id, version_id])
+            self.__handler([model_id, version_id], 'url')
         else:
-            self.__handler([model_id])
+            self.__handler([model_id], 'url')
 
-        # file_version_id = re.search(r'(?<=models\/)\d+', file['downloadUrl'])
-        # if file_version_id != None and file_version_id.group(0) == metadata.version_id:
-        #     filename = file['name']
-        #     break
+    def __api_url_handler(self):
+        version_id_regex = r'(?<=models\/)\d+'
+        version_id = re.search(version_id_regex, self.__id_string)
+        err_400_if_true(version_id == None,
+                        f'Incorrect format for the url provided: {self.__id_string}')
+        version_id = version_id.group(0)
+        err_500_if_true(
+            version_id == None, f'Unknown error while parsing version id for the url provided: {self.__id_string}')
 
-    def __handler(self, id):
-        if len(id) == 2:
-            self.model_id = id[0]
-            self.version_id = id[1]
+        self.__handler([version_id], 'api')
+
+    def __handler(self, ids: List[int], type: str):
+        if type == 'url' and len(ids) == 2:
+            self.model_id = ids[0]
+            self.version_id = ids[1]
             self.model_dict = self.__get_model_metadata()
             self.version_dict = self.__get_version_metadata()
-        elif len(id) == 1:
-            self.model_id = id[0]
+        elif type == 'id' or (type == 'url' and len(ids) == 1):
+            self.model_id = ids[0]
             self.model_dict = self.__get_model_metadata()
             err_404_if_true(len(self.model_dict['modelVersions']) == 0,
                             f'No model versions found from model id, {self.model_id}')
             self.version_dict = self.model_dict['modelVersions'][0]
-            self.version_id = self.version_dict['id']
+            self.version_id = str(self.version_dict['id'])
+        elif type == 'api':
+            self.version_id = ids[0]
+            self.version_dict = self.__get_version_metadata()
+            self.model_id = str(self.version_dict['modelId'])
+            self.model_dict = self.__get_model_metadata()
         else:
             err_400_if_true(True,
-                            f'Incorrect format sent: "{self.__id_string}"')
+                            f'Incorrect format sent ({ids}, {type}): "{self.__id_string}"')
 
         self.download_url = self.version_dict['downloadUrl']
         self.images_dict_li = self.version_dict['images']
@@ -95,17 +106,8 @@ class Metadata:
     def __get_metadata(self, url: str):
         meta_res = requests.get(url)
         err_if_true(meta_res.status_code != 200,
-                    f'Downloading metadata from CivitAI for "{self.__id_string}" failed', meta_res.status_code)
+                    f'Downloading metadata from CivitAI for "{self.__id_string}" failed when trying to request metadata from "{url}"', meta_res.status_code)
         return meta_res.json()
-
-
-# def _get_metadata_json(id: str):
-#     """Returns json object if request succeeds, else print error and returns None"""
-#     metadata_url = 'https://civitai.com/api/v1/models/' + id
-#     meta_res = requests.get(metadata_url)
-#     err_if_true(meta_res.status_code != 200,
-#                 f'Downloading metadata from CivitAI for id, {id}, failed', meta_res.status_code)
-#     return meta_res.json()
 
 
 def _download_image(dirpath: str, images: list[Dict], nsfw: bool, max_img_count):
