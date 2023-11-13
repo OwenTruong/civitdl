4a5930c8239854d5da70b541145fc5dafc0086d6
OwenTruong <44269025+OwenTruong@users.noreply.github.com>
2023-11-13 13:58:45 -0500

    added traceback for dev mode and added custom error

--- src/civitdl/batch_download.py
+++ src/civitdl/batch_download.py
@@ -1,13 +1,13 @@
-import ast
 import os
 import time
-from traceback import print_exc
-from termcolor import colored
+import traceback
+
 
 from .get_model import download_model
 from .filters import choose_filter_helper
-from .utils import err_400_if_true, err_404_if_true, err_500_if_true, err_501_if_true, err_if_true, parse_args
+from .utils import parse_args, run_in_dev
 
+from .exceptions import InputException, UnexpectedException
 
 args_message = {
     'batchfile': {
@@ -31,28 +31,50 @@ def get_input_strings(type: str, source: str):
     return res
 
 
+# TODO: Add error checking for the kwargs
+
+def verify_custom_filter():
+    None
+
+
+def verify_filter():
+    None
+
+
+def verify_max_images():
+    None
+
+
 def batch_download(type: str, argv: list[str]):
     """Accepted type are batchfile and batchstr"""
-    err_500_if_true(type != 'batchfile' and type != 'batchstr',
-                    f'Unknown batch type --> {type}')
+    if type != 'batchfile' and type != 'batchstr':
+        raise UnexpectedException(f'Unknown batch type --> {type}')
 
     message = args_message[type]
     kwargs, args = parse_args(argv)
-    err_400_if_true(len(args) < 2,
-                    f'Missing arguments. Arguments are the following: {message["args_format"]}.\nExample: {message["example"]}')
-    err_404_if_true(type != 'batchstr' and not os.path.exists(
-        args[0]), 'Source directory does not exist.')
-
     input_li = None
+    filter_model = None
+
+    if len(args) < 2:
+        raise InputException(
+            'Missing arguments.',
+            f'Arguments should be in the following format: {message["args_format"]}.', f'Example: {message["example"]}'
+        )
+    elif type == 'batchfile' and not os.path.exists(args[0]):
+        raise InputException(f'Batch file does not exist at path: {args[0]}')
+
     input_li = get_input_strings(type, args[0])
 
-    err_500_if_true(input_li == None, 'input_li is of type None')
-    err_500_if_true(len(input_li) == None,
-                    f'get_input_strings was not able to parse argument, {args[0]}')
+    if input_li == None:
+        raise UnexpectedException('input_li is of type None')
+    elif len(input_li) == 0:
+        raise UnexpectedException(
+            f'get_input_strings was not able to parse argument: {args[0]}')
 
     filter_model = choose_filter_helper(kwargs)
-    err_500_if_true(filter_model == None,
-                    'Error: filter_model is of type None')
+
+    if (filter_model == None):
+        raise UnexpectedException('filter_model is of type None')
 
     for input_str in input_li:
         try:
@@ -62,12 +84,9 @@ def batch_download(type: str, argv: list[str]):
                 dst_root_path=args[1],
                 download_image=True,
                 max_img_count=(kwargs['max-images'] if 'max-images' in kwargs else 3))
-            time.sleep(2)
+            time.sleep(3)
         except Exception as e:
-            try:
-                res = ast.literal_eval(str(e))
-                print(
-                    f"{colored('Status ' + str(res['status']), 'red', attrs=['bold'])}: {colored(res['message'], 'red')}")
-            except:
-                # TODO: What do we do about error status from civitai's server?
-                print_exc(e)
+            print('---------')
+            run_in_dev(traceback.print_exc)
+            print(e)
+            print('---------')
--- src/civitdl/command_line.py
+++ src/civitdl/command_line.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python3
 
-import ast
 import sys
-from traceback import print_exc
-from termcolor import colored
+import traceback
 
-from .utils import err_400_if_true
+from .utils import run_in_dev
 from .batch_download import batch_download
+from .exceptions import InputException
 
 
 def main():
     try:
         available_actions = ['batchfile', 'batchstr']
         argv = sys.argv[1:]
-        err_400_if_true(len(argv) == 0,
-                        f'Error: Need to provide arguments -> <action> *<action\'s arguments>.\nAvailable actions: {available_actions}')
+        if len(argv) == 0:
+            raise InputException(
+                f'Need to provide arguments -> <action> *<action\'s arguments>.', f'Available actions: {available_actions}'
+            )
 
         if argv[0] == 'batchfile':
             batch_download('batchfile', argv[1:])
         elif argv[0] == 'batchstr':
             batch_download('batchstr', argv[1:])
         else:
-            err_400_if_true(
-                True, TypeError, f'Error: Unknown action.\nAvailable actions: {available_actions}')
+            raise InputException(
+                f'Unknown action.',
+                f'Available actions: {available_actions}'
+            )
     except Exception as e:
-        try:
-            res = ast.literal_eval(str(e))
-            print(
-                f"{colored('Status ' + str(res['status']), 'red', attrs=['bold'])}: {colored(res['message'], 'red')}")
-        except:
-            # TODO: What do we do about error status from civitai's server?
-            print_exc(e)
+        print('---------')
+        run_in_dev(traceback.print_exc)
+        print(e)
+        print('---------')
